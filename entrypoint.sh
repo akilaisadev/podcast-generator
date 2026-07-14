6d8dfde24465e616cdd34a86a3c736970009379f
Akila Wasalathilaka <akilawasalathilaka@gmail.com>
2026-07-14 16:46:30 +0000

    entrypoint add fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,12 @@ echo "==============="
 
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
-git config --global --add save.directory  /github/workspace
+git config --global --add safe.directory /github/workspace
 
-python /urs/bin/feed.py
+python /usr/bin/feed.py
 
-git add -A && git commit -m "Update Feed"
+git add -A
+git commit -m "Update Feed"
 
 git push --set-upstream origin main
 
